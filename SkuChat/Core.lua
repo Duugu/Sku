e43a6c66273678eed4059184f13cf97c0286ca56
Duugu <65570458+Duugu@users.noreply.github.com>
2022-11-02 13:41:27 +0100

    r31.16

--- SkuChat/Core.lua
+++ SkuChat/Core.lua
@@ -2059,13 +2059,11 @@ function SkuChat:DEFAULT_CHAT_FRAME_AddMessage(...)
 		return
 	end
 	--SkuCore:Debug((a or "nil").." "..(b or "nil").." "..(c or "nil").." "..(d or "nil").." "..(e or "nil").." "..(f or "nil"))
-	if SkuOptions.db.profile["SkuChat"].tabs then
-		if SkuOptions.db.profile["SkuChat"].tabs[1] then
-			if _G[SkuOptions.db.profile["SkuChat"].tabs[1].frameName] then
-				if _G[SkuOptions.db.profile["SkuChat"].tabs[1].frameName].AddMessage then
-					if b == nil and c == nil and  d == nil and  e == nil and  f == nil then
-						_G[SkuOptions.db.profile["SkuChat"].tabs[1].frameName]:AddMessage("ADDON", a, 1, 1, 1, 0, 0, 0, "AddMessage")
-					end
+	for i, v in pairs(SkuOptions.db.profile["SkuChat"].tabs) do
+		if _G[v.frameName] then
+			if _G[v.frameName].AddMessage then
+				if b == nil and c == nil and  d == nil and  e == nil and  f == nil then
+					_G[v.frameName]:AddMessage("ADDON", a, 1, 1, 1, 0, 0, 0, "AddMessage")
 				end
 			end
 		end
